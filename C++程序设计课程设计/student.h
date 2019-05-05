79d2a620ca94db18492f71ca6962841efe05cad3
jmh <1099461301@qq.com>
2019-05-05 23:16:40 +0800

    mini实验设计

---
+++ C++程序设计课程设计/student.h
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class student 
+{
+	public:
+		void get_info();
+		
+	private:
+		string name;
+		int year;
+		int month;
+		int date;
+		int order;
+		string classname;
+		string abstype;
+		
+} 
